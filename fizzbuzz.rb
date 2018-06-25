8636615cbf850b5ec9117c3682a07070258697ed
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 04:15:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 3 == 0
-    puts "Fizz"
+    "Fizz"
    elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
    elsif int % 15 == 0
-     puts "FizzBuzz"
+     "FizzBuzz"
    else
 
     end
