f85ab553754cda922311bd6369a432a99df97911
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-25 02:44:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz
+  
+  if number == %3
+    puts "Fizz"
+  elsif number == %5
+    puts "Buzz"
+  elsif number == %3 && ==%5
+    puts "FizzBuzz"
+  else
+    puts "nil"
+    
+  end
+  
+end
